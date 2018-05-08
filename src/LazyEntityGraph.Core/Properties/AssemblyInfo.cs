e6e36e920709c65191e7b58d396a49006861cc16
David Broudy <dave@topya.com>
2018-05-08 13:42:33 -0600

    Update version and integrate appveyor

--- src/LazyEntityGraph.Core/Properties/AssemblyInfo.cs
+++ src/LazyEntityGraph.Core/Properties/AssemblyInfo.cs
@@ -7,9 +7,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("LazyEntityGraph.Core")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Alex Fox Gill")]
+[assembly: AssemblyCompany("David Broudy")]
 [assembly: AssemblyProduct("LazyEntityGraph.Core")]
-[assembly: AssemblyCopyright("Copyright © Alex Fox Gill 2016")]
+[assembly: AssemblyCopyright("Copyright © 2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
