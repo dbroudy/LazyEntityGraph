d2ff3e1844d1f18586b44dcdfd54315f0e6761cb
David Broudy <dave@topya.com>
2018-10-10 15:02:47 -0600

    Add EF Core support and tests (#11). Model changed to support EF Core.

---
+++ src/LazyEntityGraph.EntityFrameworkCore.Tests/EndToEndTests.cs
@@ -0,0 +1,48 @@
+﻿using FluentAssertions;
+using LazyEntityGraph.AutoFixture;
+using AutoFixture;
+using AutoFixture.Xunit2;
+using Xunit;
+using System;
+
+namespace LazyEntityGraph.EntityFrameworkCore.Tests
+{
+    public class BlogModelDataAttribute : AutoDataAttribute
+    {
+        public BlogModelDataAttribute() : base(CreateFixture)
+        {
+        }
+
+        private static IFixture CreateFixture()
+        {
+            return new Fixture()
+                .Customize(new LazyEntityGraphCustomization(
+                                ModelMetadataGenerator.LoadFromContext<BlogContext>(options => new BlogContext(options))));
+        }
+    }
+
+    public class EndToEndTests
+    {
+        [Theory, BlogModelData]
+        public void ForeignKeyPropertyOnOneToMany(Post post)
+        {
+            // assert
+            post.Poster.Id.Should().Be(post.PosterId);
+        }
+
+        [Theory, BlogModelData]
+        public void ForeignKeyPropertyOnManyToOne(User user)
+        {
+            // assert
+            foreach (var post in user.Posts)
+                post.PosterId.Should().Be(user.Id);
+        }
+
+        [Theory, BlogModelData]
+        public void ForeignKeyPropertyOnDerivedOneToMany(Story story)
+        {
+            // assert
+            story.Poster.Id.Should().Be(story.PosterId);
+        }
+    }
+}
--- src/LazyEntityGraph.EntityFrameworkCore.Tests/ModelMetadataGeneratorTests.cs
+++ src/LazyEntityGraph.EntityFrameworkCore.Tests/ModelMetadataGeneratorTests.cs
@@ -1,17 +1,17 @@
 ﻿using FluentAssertions;
 using LazyEntityGraph.Core;
 using LazyEntityGraph.Core.Constraints;
-using LazyEntityGraph.EntityFramework;
+using LazyEntityGraph.EntityFrameworkCore;
 using System.Collections;
 using Xunit;
 
-namespace LazyEntityGraph.EntityFramework.Tests
+namespace LazyEntityGraph.EntityFrameworkCore.Tests
 {
     public class ModelMetadataGeneratorTests
     {
         public static ModelMetadata GetMetadata()
         {
-            return ModelMetadataGenerator.LoadFromCodeFirstContext(str => new BlogContext(str), false);
+            return ModelMetadataGenerator.LoadFromContext<BlogContext>(options => new BlogContext(options));
         }
 
         [Fact]
@@ -20,7 +20,7 @@ namespace LazyEntityGraph.EntityFramework.Tests
             // arrange
             var expected = new[]
             {
-                typeof (Post), typeof (User), typeof (Tag), typeof (ContactDetails), typeof (Category), typeof(Story)
+                typeof (Post), typeof (User), typeof (ContactDetails), typeof (Category), typeof(Story)
             };
 
             // act
@@ -37,8 +37,6 @@ namespace LazyEntityGraph.EntityFramework.Tests
             // arrange
             var expected = new IPropertyConstraint[]
             {
-                new ManyToManyPropertyConstraint<Post, Tag>(p => p.Tags, t => t.Posts),
-                new ManyToManyPropertyConstraint<Tag, Post>(t => t.Posts, p => p.Tags),
                 new OneToManyPropertyConstraint<User, Post>(u => u.Posts, p => p.Poster),
                 new ManyToOnePropertyConstraint<Post, User>(p => p.Poster, u => u.Posts),
                 new OneToOnePropertyConstraint<User,ContactDetails>(u => u.ContactDetails, c => c.User),
